464dfb61938cdfcca91b8e3099c1ce97b4a488d0
Robert Cseke <cseke.robi12@gmail.com>
2024-12-03 17:21:51 +0000

    Adjusting navbar and footer

--- src/js/script.js
+++ src/js/script.js
@@ -11,18 +11,3 @@ document.addEventListener('click', function (event) {
       navbarToggler.click(); // close the menu on outside clicks
     }
   });
-
-  // script for form validation
-  document.querySelector('form').addEventListener('submit', function (e) {
-    e.preventDefault(); // prevent form submission
-    const name = document.getElementById('name').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const message = document.getElementById('message').value.trim();
-  
-    if (name && email && message) {
-      alert('Thank you for contacting us!');
-      //send form data to a server
-    } else {
-      alert('Please fill in all fields.');
-    }
-  });
